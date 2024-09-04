978e1cd2f9a8fa8944f60eab90b9aa6ad5e28d83
ParshadKyada <parshadkyada2003@gmail.com>
2024-09-04 19:57:36 +0530

    first commit

---
+++ frontend/src/components/protect/HiddenLink.js
@@ -0,0 +1,14 @@
+import { useSelector } from "react-redux";
+import { selectIsLoggedIn } from "../../redux/features/auth/authSlice";
+
+export const ShowOnLogin = ({ children }) => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
+  return isLoggedIn ? <>{children}</> : null;
+};
+
+export const ShowOnLogout = ({ children }) => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
+  return !isLoggedIn ? <>{children}</> : null;
+};
---
+++ frontend/src/redux/features/auth/authSlice.js
@@ -0,0 +1,58 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// Retrieve 'name' from localStorage
+const storedName = localStorage.getItem("name");
+let name = "";
+if (storedName) {
+  try {
+    name = JSON.parse(storedName);
+  } catch (error) {
+    console.error("Error parsing 'name' from localStorage:", error);
+  }
+}
+
+const initialState = {
+  isLoggedIn: false,
+  name: name,
+  user: {
+    name: "",
+    email: "",
+    phone: "",
+    bio: "",
+    photo: "",
+  },
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    SET_LOGIN(state, action) {
+      state.isLoggedIn = action.payload;
+    },
+    SET_NAME(state, action) {
+      try {
+        localStorage.setItem("name", JSON.stringify(action.payload));
+        state.name = action.payload;
+      } catch (error) {
+        console.error("Error setting 'name' in localStorage:", error);
+      }
+    },
+    SET_USER(state, action) {
+      const profile = action.payload;
+      state.user.name = profile.name;
+      state.user.email = profile.email;
+      state.user.phone = profile.phone;
+      state.user.bio = profile.bio;
+      state.user.photo = profile.photo;
+    },
+  },
+});
+
+export const { SET_LOGIN, SET_NAME, SET_USER } = authSlice.actions;
+
+export const selectIsLoggedIn = (state) => state.auth.isLoggedIn;
+export const selectName = (state) => state.auth.name;
+export const selectUser = (state) => state.auth.user;
+
+export default authSlice.reducer;
